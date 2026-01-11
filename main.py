ad6ca7c215af0630cf499642c4d94f487a8e10ee
rasulkimsanbaiuulu <rasulkimsanbevich@gmail.com>
2026-01-12 00:40:58 +0600

    задания 1 2

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from datetime import datetime
 
 def main(page: ft.Page):
     page.title="flet project"
-    page.theme_mode = ft.ThemeMode.LIGHT
-
-    
-    
-  
+    page.theme_mode = ft.ThemeMode.LIGHT  
     text = ft.Text(value="Hello world!")
+
+    greeting_history = []
+    history_text = ft.Text("История приветствий:")
 
 
     # functions
@@ -16,8 +15,13 @@ def main(page: ft.Page):
         date =datetime.now().strftime("%d.%m.%Y - %H:%M:%S")
         name = text_field.value
         if name:
+            text_field.value = None
             text.color = None
             text.value = f'{date} - Hello, {name.title()}!'
+
+            greeting_history.append(name)
+            print(greeting_history)
+            history_text.value = "История приветствий: \n" + ", \n" .join(greeting_history)
         else:
             text.value = "Enter name!"
             text.color = ft.Colors.RED    
@@ -28,16 +32,35 @@ def main(page: ft.Page):
         else:
             page.theme_mode = ft.ThemeMode.LIGHT
 
-    # buttons
+    def clear_button(e):
+        print(greeting_history)
+        greeting_history.clear()
+        print(greeting_history)
+        history_text.value = "История приветствий: "
+
+    def sorte(_):
+        greeting_history.sort()
+        history_text.value = "История приветствий: \n" + ", \n" .join(greeting_history)
+    def delete_f(_):
+        if greeting_history:
+            del greeting_history[-1:]
+            print(greeting_history)
+            history_text.value = "История приветствий: \n" + ", \n" .join(greeting_history)
+        else: 
+            history_text.value = "История пуста"    
+
+    # buttons   
     elevated_button = ft.ElevatedButton("SEND", on_click=name_update)
-    text_field = ft.TextField(label="Please enter name")
+    text_field = ft.TextField(label="Please enter name", expand=True)
     icon_button = ft.IconButton(icon=ft.Icons.FLARE_SHARP, on_click=change_page)
+    clear_buttonn = ft.IconButton(icon=ft.Icons.DELETE, on_click=clear_button)
+    sort = ft.IconButton(icon=ft.Icons.SORT, on_click=sorte) 
+    delete_last = ft.ElevatedButton("del last", on_click=delete_f)
 
 
+    my_object = ft.Row([text_field, elevated_button, icon_button, clear_buttonn, delete_last])
+    
 
 
-
-
-
-    page.add(text, text_field, elevated_button, icon_button)
+    page.add(text, my_object, sort, history_text)
 ft.app(target=main)    
